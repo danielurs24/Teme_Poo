31aaff7a4d66a6096b6b4311df2481a3b7b4ddde
danielurs24 <73581144+danielurs24@users.noreply.github.com>
2020-10-31 16:59:38 +0200

    Tema 1

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ private:
 public:
 
     Masina(const string &marca, const string &model, const string &culoare, const int &pret) {
+
         this->marca=marca;
         this->model=model;
         this->culoare=culoare;
@@ -22,45 +23,64 @@ public:
     }
 
     Masina(){
+
         this->marca="FaraMarca";
         this->model="FaraModel";
         this->culoare="FaraCuloare";
         this->pret=0;
     }
 
+    Masina(const Masina &mas) {
+
+        this->marca=mas.marca;
+        this->model=mas.model;
+        this->culoare=mas.culoare;
+        this->pret=mas.pret;
+
+    }
+
     const string &getMarca() const {
+
         return marca;
     }
 
     void setMarca(const string &marca) {
+
         Masina::marca = marca;
     }
 
     const string &getModel() const {
+
         return model;
     }
 
     void setModel(const string &model) {
+
         Masina::model = model;
     }
 
     const string &getCuloare() const {
+
         return culoare;
     }
 
     void setCuloare(const string &culoare) {
+
         Masina::culoare = culoare;
     }
 
     int getPret() const {
+
         return pret;
     }
 
     void setPret(int pret) {
+
         Masina::pret = pret;
     }
 
     virtual ~Masina() {
+
         cout << "Masina " << this->marca << " " << this->model << " a fost reparata" << "\n";
 
     }
@@ -70,6 +90,7 @@ public:
 };
 
 void Masina::inService(){
+
     cout << "Masina " << marca << " " << model << " de culoare " << culoare << " cu pretul de " << pret << "$ a intrat in service " << "\n";
 };
 
@@ -84,59 +105,72 @@ private:
 public:
 
     service(const string &locatie = "Focsani",const string &manager = "Manager", const int &locuriMasini = 2)  {
+
     this->locatie=locatie;
     this->locuri_masini=locuriMasini;
     this->manager=manager;
 
 }
     service(const service &srv) {
-        manager = srv.manager;
-        locatie = srv.locatie;
-        locuri_masini = srv.locuri_masini;
+
+        this->manager = srv.manager;
+        this->locatie = srv.locatie;
+        this->locuri_masini = srv.locuri_masini;
 
     }
 
     const string &getLocatie() const {
+
         return locatie;
     }
     void setLocatie(const string &locatie) {
+
         service::locatie = locatie;
     }
 
     const string &getManager() const {
+
         return manager;
     }
 
     void setManager(const string &manager) {
+
         service::manager = manager;
     }
 
     int getLocuriMasini() const {
+
         return locuri_masini;
     }
 
     void setLocuriMasini(int locuriMasini) {
+
         locuri_masini = locuriMasini;
     }
 
     virtual ~service() {
+
         cout << "Service-ul din " << locatie << " s-a inchis" << "\n";
     }
     int seRepara();
 };
 
-    int service::seRepara()
-    {
+    int service::seRepara(){
+
         return locuri_masini--;
     }
-class reparatie
-{
+class reparatie{
+
 private:
+
     string denumire;
     int durata;
     int cost;
+
 public:
+
     reparatie(const string &denumire, int durata, int cost) {
+
         this->denumire=denumire;
         this->durata=durata;
         this->cost=cost;
@@ -144,29 +178,36 @@ public:
 
     virtual ~reparatie() {
 
+
     }
 
     const string &getDenumire() const {
+
         return denumire;
     }
 
     void setDenumire(const string &denumire) {
+
         reparatie::denumire = denumire;
     }
 
     int getDurata() const {
+
         return durata;
     }
 
     void setDurata(int durata) {
+
         reparatie::durata = durata;
     }
 
     int getCost() const {
+
         return cost;
     }
 
     void setCost(int cost) {
+
         reparatie::cost = cost;
     }
 };
@@ -174,6 +215,7 @@ public:
 
 
 int main() {
+
     service service1("Bucuresti","Alex",5);
     service service2("Cluj","Marius");
 
@@ -190,10 +232,19 @@ int main() {
     Masina masina1("BMW","X5","Negru",5000);
     Masina masina2("Audi","A3","Verde",4000);
 
+    reparatie repMasina1("Revizie", 40, 700);
+    reparatie repMasina2("Schimb Placute de frana", 30, 350);
+
     masina1.inService();
     masina2.inService();
+
     service1.seRepara();
+
+    cout << masina1.getMarca() << " " << masina1.getModel() << " este in service pentru a efectua " << repMasina1.getDenumire() << " \n";
+
     service1.seRepara();
+
+    cout << masina2.getMarca() << " " << masina2.getModel() << " este in service pentru a efectua " << repMasina2.getDenumire() << " \n";
 
     cout << "\nIn service-ul din " << service1.getLocatie() << " mai sunt " << service1.getLocuriMasini() << " locuri\n\n";
 
